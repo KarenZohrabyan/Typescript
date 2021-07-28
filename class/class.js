ba97b9cfbcb06f072fe650b03e85705094f40531
karenzohrabyan <zohrabyankaren96@gmail.com>
2021-07-28 15:45:05 +0400

    Class inheritance

--- class/class.js
+++ class/class.js
@@ -1,6 +1,37 @@
+// class Department {
+//     public readonly name: string
+//     private age: number;
+//     constructor(n: string, age: number) {
+//         this.name = n;
+//         this.age = age;
+//     }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//     describe() {
+//         console.log(`My name is ${this.name}`);
+//         console.log(`My age is ${this.age}`);
+//     }
+// }
 var Department = /** @class */ (function () {
-    function Department(n, age) {
-        this.name = n;
+    // public readonly name: string
+    // private age: number;
+    function Department(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
         this.age = age;
     }
     Department.prototype.describe = function () {
@@ -9,11 +40,34 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-try {
-    var IT = new Department("Karen", 25);
-    IT.describe();
-    console.log(IT.name);
-}
-catch (error) {
-    throw new Error("-----------------");
-}
+var MyDeparment = /** @class */ (function (_super) {
+    __extends(MyDeparment, _super);
+    function MyDeparment(name, admins) {
+        var _this = _super.call(this, name, 25) || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return MyDeparment;
+}(Department));
+var MyAccount = /** @class */ (function (_super) {
+    __extends(MyAccount, _super);
+    function MyAccount(name, reports) {
+        var _this = _super.call(this, name, 34) || this;
+        _this.reports = reports;
+        return _this;
+    }
+    MyAccount.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    MyAccount.prototype.getReports = function () {
+        console.log(this.reports);
+    };
+    return MyAccount;
+}(Department));
+var account = new MyAccount("AAAAAA", []);
+account.addReport('asdasdasdas');
+account.getReports();
+var IT = new Department("Karen", 25);
+IT.describe();
+var obj = { name: 'asd', describe: IT.describe };
+obj.describe();
--- class/class.ts
+++ class/class.ts
@@ -1,8 +1,22 @@
+// class Department {
+//     public readonly name: string
+//     private age: number;
+//     constructor(n: string, age: number) {
+//         this.name = n;
+//         this.age = age;
+//     }
+
+//     describe() {
+//         console.log(`My name is ${this.name}`);
+//         console.log(`My age is ${this.age}`);
+//     }
+// }
+
 class Department {
-    public readonly name: string
-    private age: number;
-    constructor(n: string, age: number) {
-        this.name = n;
+    // public readonly name: string
+    // private age: number;
+    constructor(public name: string, private readonly age: number) {
+        this.name = name;
         this.age = age;
     }
 
@@ -12,15 +26,35 @@ class Department {
     }
 }
 
-class MyDepartment extends Department {
-
+class MyDeparment extends Department {
+    admins: string[];
+    constructor(name: string, admins: string[]) {
+        super(name, 25);
+        this.admins = admins;
+    }
 }
 
-try {
-    const IT = new Department("Karen", 25);
-    IT.describe();
+class MyAccount extends Department {
+    constructor(name: string, private reports: string[]) {
+        super(name, 34);
+    }
 
-    console.log(IT.name);
-} catch (error) {
-    throw new Error("-----------------")
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    getReports() {
+        console.log(this.reports);
+    }
 }
+
+const account = new MyAccount("AAAAAA", []);
+account.addReport('asdasdasdas');
+account.getReports();
+
+
+const IT = new Department("Karen", 25);
+IT.describe();
+
+const obj = {name: 'asd', describe: IT.describe}
+obj.describe()
